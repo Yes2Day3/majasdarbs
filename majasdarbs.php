44a6ea8f2d411ed137275c6d348460bd31f54c4e
you@example.com <you@example.com>
2026-01-28 10:12:53 +0200

    Izpildīts piektais uzdevums

--- majasdarbs.php
+++ majasdarbs.php
@@ -10,24 +10,88 @@
 
 // echo $ages["Bob"];
 
-$students = [
-    ["name"=>"John", "age"=>20, "grades" => [90,85,88]],
-    ["name"=>"Jane", "age"=>22, "grades" => [92,80,84]],
-    ["name"=>"Joe", "age"=>20, "grades" => [78,85,90]],
+// $students = [
+//     ["name"=>"John", "age"=>20, "grades" => [90,85,88]],
+//     ["name"=>"Jane", "age"=>22, "grades" => [92,80,84]],
+//     ["name"=>"Joe", "age"=>20, "grades" => [78,85,90]],
+// ];
+
+// foreach($students as $student){
+//     $sum = 0;
+//     for($i = 0; $i < 3; $i++){
+//         $sum += $student["grades"][$i];
+//     }
+//     echo $student["name"] . " " . $sum / count($student["grades"]) . "\n";
+// }
+
+// foreach($students as $student){
+//     echo "Name: {$student["name"]} Age: {$student["age"]} Grades: ";
+//     for($i = 0; $i < 3; $i++){
+//         echo $student["grades"][$i] . " ";
+//     }
+//     echo "\n";
+// }
+
+$assocAnimal = [
+    "name"=>"Lion",
+    "mass"=>250,
+    "type"=>"mammal",
+    "prey"=>[
+        [
+            "name"=>"Zebra",
+            "type"=>"mammal",
+        ],
+        [
+            "name"=>"Gazelle",
+            "type"=>"mammal",
+        ]
+    ]
 ];
 
-foreach($students as $student){
-    $sum = 0;
-    for($i = 0; $i < 3; $i++){
-        $sum += $student["grades"][$i];
-    }
-    echo $student["name"] . " " . $sum / count($student["grades"]) . "\n";
-}
-
-foreach($students as $student){
-    echo "Name: {$student["name"]} Age: {$student["age"]} Grades: ";
-    for($i = 0; $i < 3; $i++){
-        echo $student["grades"][$i] . " ";
-    }
-    echo "\n";
-}
+$animals = [
+    [
+        "name"=>"Lion",
+        "mass"=>250,
+        "type"=>"mammal",
+        "prey"=>[
+            [
+                "name"=>"Zebra",
+                "type"=>"mammal",
+            ],
+            [
+                "name"=>"Gazelle",
+                "type"=>"mammal",
+            ]
+        ]
+    ],
+    [
+        "name"=>"Cougar",
+        "mass"=>180,
+        "type"=>"mammal",
+        "prey"=>[
+            [
+                "name"=>"Zebra",
+                "type"=>"mammal",
+            ],
+            [
+                "name"=>"Gazelle",
+                "type"=>"mammal",
+            ]
+        ]
+    ],
+    [
+        "name"=>"hyena",
+        "mass"=>150,
+        "type"=>"mammal",
+        "prey"=>[
+            [
+                "name"=>"Zebra",
+                "type"=>"mammal",
+            ],
+            [
+                "name"=>"Gazelle",
+                "type"=>"mammal",
+            ]
+        ]
+    ],
+];
